1b5b19cbaf61c86e479a25398f79d07ee388b0e6
Fuzzwah <rob.crouch@gmail.com>
2024-04-04 17:32:30 +1100

    initial stab at it, based on ASD_NetColoc_analysis.ipynb example

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os	
+import os
 # load required packages
 import numpy as np
 import matplotlib.pyplot as plt
@@ -41,7 +41,7 @@ plt.rcParams['svg.fonttype'] = 'none'
 
 from datetime import datetime
 import sys
-	
+
 # verify DDOT was installed
 import ddot
 
@@ -72,16 +72,16 @@ with open('input/' + file_name, 'r') as file:
 # Remove any whitespace characters like '\n' at the end of each line
 D1_genes = [gene.strip() for gene in D1_genes]
 print("First 5 genes:", D1_genes[0:5])
-	
+
 # interactome_uuid='4de852d9-9908-11e9-bcaf-0ac135e8bacf' # for PCNet
 interactome_uuid='275bd84e-3d18-11e8-a935-0ac135e8bacf' # for STRING high confidence
 ndex_server='public.ndexbio.org'
 ndex_user=None
 ndex_password=None
 G_int = ndex2.create_nice_cx_from_server(
-            ndex_server, 
-            username=ndex_user, 
-            password=ndex_password, 
+            ndex_server,
+            username=ndex_user,
+            password=ndex_password,
             uuid=interactome_uuid
         ).to_networkx()
 nodes = list(G_int.nodes)
@@ -109,8 +109,8 @@ print("Number of D1 genes in interactome:", len(D1_genes))
 
 # D1 network propagation
 print('\nCalculating D1 z-scores: ')
-z_D1, Fnew_D1, Fnew_rand_D1 = netprop_zscore.calculate_heat_zscores(w_double_prime, int_nodes, 
-                                                                    dict(G_int.degree), 
+z_D1, Fnew_D1, Fnew_rand_D1 = netprop_zscore.calculate_heat_zscores(w_double_prime, int_nodes,
+                                                                    dict(G_int.degree),
                                                                     D1_genes, num_reps=1000,
                                                                     minimum_bin_size=100)
 
@@ -123,7 +123,7 @@ zthresh=3 # default = 3
 
 # select the genes in the network intersection, make a subgraph
 
-G_prox = nx.subgraph(G_int,z_D1[z_D1['z']>zthresh].index.tolist()) 
+G_prox = nx.subgraph(G_int,z_D1[z_D1['z']>zthresh].index.tolist())
 print("Nodes in proximal subgraph:", len(G_prox.nodes()))
 print("Edges in proximal subgraph:", len(G_prox.edges()))
 
@@ -139,12 +139,11 @@ node_df['sum_seeds']=node_df['d1_seeds']
 node_df = node_df.sort_values('z_d1',ascending=False)
 node_df.head(15)
 
-	
 print("Nodes in overlap subgraph:", len(G_prox.nodes()))
 print("Edges in overlap subgraph:", len(G_prox.edges()))
 # Create cx format of overlap subgraph
 G_prox_cx = ndex2.create_nice_cx_from_networkx(G_prox)
-G_prox_cx.set_name(d1_name+'_NetColoc_subgraph') 
+G_prox_cx.set_name(d1_name+'_NetColoc_subgraph')
 for node_id, node in G_prox_cx.get_nodes():
     data = node_df.loc[node['n']]
     for row, value in data.items():
@@ -167,7 +166,6 @@ print('# nodes: ' + str(len(G_hier.get_nodes())))
 print('# edges: ' + str(len(G_hier.get_edges())))
 
 G_hier = G_hier.to_networkx(mode='default')
-G_hier
 
 nodes = G_hier.nodes()
 
@@ -175,7 +173,6 @@ nodes = G_hier.nodes()
 print('Number of nodes:', len(G_hier.nodes()))
 print('\nNumber of edges:', len(G_hier.edges()))
 
-	
 # add node attributes to dataframe for easier access
 hier_df = pd.DataFrame.from_dict(dict(G_hier.nodes(data=True)), orient='index')
 hier_df['system_ID']=hier_df.index.tolist()
@@ -199,10 +196,9 @@ for c in hier_df.index.tolist():
     if (num_d1_temp)>0: # keep the system if it has at least 1 seed gene
         systems_keep.append(c)
         num_d1_seeds.append(num_d1_temp)
-        
+
         frac_d1_seeds.append((num_d1_temp)/float(len(system_genes)))
 
-        
 frac_no_seeds = np.subtract(1.0,np.array([frac_d1_seeds]).sum(axis=0))
 
 hier_df = hier_df.loc[systems_keep]
@@ -213,22 +209,18 @@ print("Number of nodes with seed genes:", len(hier_df))
 
 hier_df.head()
 
-	
 # prune G_hier--> only keep systems with at least one seed gene
 
 nkeep=[]
 for n in list(G_hier.nodes()):
     if G_hier.nodes(data=True)[n]['name'] in systems_keep:
         nkeep.append(n)
-        
 
 G_hier = nx.subgraph(G_hier, nkeep)
 print("Number of nodes with seed genes:", len(G_hier.nodes()))
 print("Number of edges remaining:", len(G_hier.edges()))
 
-	
 # gprofiler annotation of clusters
-
 system_name_list = []
 for p in hier_df.index.tolist():
     focal_genes=hier_df['CD_MemberList'].loc[p].split(' ')
@@ -238,40 +230,34 @@ for p in hier_df.index.tolist():
         gp_temp = pd.DataFrame(gp.profile(focal_genes,significance_threshold_method='fdr',
                                                sources=['REAC']))
         if len(gp_temp)>0: # make sure data is not empty
-            
+
             # make sure terms are specific, and overlap with at least 3 genes
             gp_temp = gp_temp[(gp_temp['term_size']<1000)&(gp_temp['term_size']>50)]
             gp_temp = gp_temp[gp_temp['intersection_size']>=3]
-            
+
             gp_temp = gp_temp[gp_temp['p_value']<1E-5] # set a stringent pvalue threshold
-            
+
             gp_temp = gp_temp.sort_values('recall',ascending=False)
-            
+
             if len(gp_temp)>1:
                 system_name_list.append(gp_temp.head(1)['name'].tolist()[0])
             else:
                 system_name_list.append(p)
         else:
             system_name_list.append(p)
-            
-        display(gp_temp.head())
-        
+
     else:
         system_name_list.append(p)
 
-	
+
 mgi_df = validation.load_MGI_mouseKO_data()
 mgi_df.head()
 
-	
 MPO = validation.load_MPO()
-MPO
 
-	
 def get_MP_description(term_id, ontology=MPO):
     return ontology.node_attr.loc[term_id, "description"]
 
-	
 # find terms related to brain
 # ---- modify this part as needed for your project -----
 MP_focal_brain_list = []
@@ -283,20 +269,19 @@ for t in MPO.node_attr.index.tolist():
         MP_focal_brain_list.append(t)
     elif descr_temp.find('synapt')>-1:
         MP_focal_brain_list.append(t)
-        
+
 print("Number of brain phenotypes", len(MP_focal_brain_list))
 print("Example brain phenotypes:")
 print("\n".join([mp+" - "+get_MP_description(mp) for mp in MP_focal_brain_list[0:10]]))
 
-	
 # add a negative control phenotype: abnormal innate immunity: MP:0002419
 # negative controls are tough here because we're dealing with development, which impacts almost everything.
 MP_focal_list = ['MP:0002419']+MP_focal_brain_list
 root_KO_brain_df=validation.MPO_enrichment_root(hier_df,MPO,mgi_df,MP_focal_list,G_int,verbose=True)
 
-	
+
 root_KO_brain_df.head()
-	
+
 # join brain and heart results together
 root_KO_brain_df['MPO_term_type']='brain'
 root_KO_df = root_KO_brain_df
@@ -305,7 +290,7 @@ root_KO_df = root_KO_df.sort_values('OR_p')
 root_KO_df.head()
 root_KO_df['MPO_term_type'].value_counts()
 
-	
+
 # plot top performing brain and heart terms + negative control term (MP:0002419) (for terms which have at least 150 genes)
 
 brain_terms_plot = root_KO_brain_df[root_KO_brain_df['num_genes_in_term']>150]
@@ -315,7 +300,7 @@ neg_ctrl_terms_plot=['MP:0002419']
 
 terms_plot = brain_terms_plot +neg_ctrl_terms_plot
 
-	
+
 plt.figure(figsize=(3,6))
 
 plt.errorbar(root_KO_df.loc[terms_plot]['log_OR'],np.arange(len(terms_plot)),
