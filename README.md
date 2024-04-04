0dd1083b4dd4b9c087a52440fe88329b4a2e7a75
Fuzzwah <rob.crouch@gmail.com>
2024-04-04 17:27:03 +1100

    initial stab at it, based on ASD_NetColoc_analysis.ipynb example

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # colocalized-genes-chart
+
+## requirements
+
+The Data-Driven Ontology Toolkit (DDOT) - https://github.com/idekerlab/ddot
+NetColoc - https://github.com/ucsd-ccbb/NetColoc
+
+## setup
+
+DDOT depends on Python libs that need Python <=3.9
+
+```
+git clone https://github.com/Fuzzwah/colocalized-genes-chart.git
+cd colocalized-genes-chart
+python3.9 -m venv .env
+. .env/bin/activate
+pip install -U pip
+pip install -r requirements.txt
+git clone --branch python3 https://github.com/idekerlab/ddot.git
+cd ddot
+python setup.py build
+python setup.py install
+cd ..
+```
+
+## running
+
+Drop your list of genes into the `input` directory, then run `python main.py`. You'll be prompted with a list of the files in the `input` directory, select the one you'd like to process
---
+++ main.py
@@ -0,0 +1,340 @@
+import os	
+# load required packages
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import networkx as nx
+import pandas as pd
+import re
+import random
+from rich.prompt import Prompt
+
+from IPython.display import display
+
+import getpass
+import ndex2
+
+import json
+import cdapsutil
+
+from gprofiler import GProfiler
+gp = GProfiler("MyToolName/0.1")
+
+from scipy.stats import hypergeom
+from scipy.stats import norm
+
+# latex rendering of text in graphs
+import matplotlib as mpl
+mpl.rc('text', usetex = False)
+mpl.rc('font', family = 'serif')
+
+from matplotlib import rcParams
+rcParams['font.family'] = 'sans-serif'
+rcParams['font.sans-serif'] = ['Arial']
+
+sns.set(font_scale=1.4)
+
+sns.set_style('white')
+
+sns.set_style("ticks", {"xtick.major.size": 15, "ytick.major.size": 15})
+plt.rcParams['svg.fonttype'] = 'none'
+
+from datetime import datetime
+import sys
+	
+# verify DDOT was installed
+import ddot
+
+from netcoloc import netprop_zscore, netprop, network_colocalization, validation
+
+# use rich to prompt for the name of the geneset
+d1_name = Prompt.ask("Enter the name of the gene set", default="Gene Set 1")
+
+# Get a list of files in the directory
+files = [f for f in os.listdir('input') if f != '.gitkeep']
+
+# Print the files with a number option
+for i, file in enumerate(files, start=1):
+    print(f"{i}. {file}")
+
+# Prompt the user to select a file
+file_number = Prompt.ask("Please enter the number of the file to process", choices=[str(i) for i in range(1, len(files) + 1)])
+
+# Get the selected file
+file_name = files[int(file_number) - 1]
+
+# load the gene set from the file
+# Open the file in read mode
+with open('input/' + file_name, 'r') as file:
+    # Read all lines into a list
+    D1_genes = file.readlines()
+
+# Remove any whitespace characters like '\n' at the end of each line
+D1_genes = [gene.strip() for gene in D1_genes]
+print("First 5 genes:", D1_genes[0:5])
+	
+# interactome_uuid='4de852d9-9908-11e9-bcaf-0ac135e8bacf' # for PCNet
+interactome_uuid='275bd84e-3d18-11e8-a935-0ac135e8bacf' # for STRING high confidence
+ndex_server='public.ndexbio.org'
+ndex_user=None
+ndex_password=None
+G_int = ndex2.create_nice_cx_from_server(
+            ndex_server, 
+            username=ndex_user, 
+            password=ndex_password, 
+            uuid=interactome_uuid
+        ).to_networkx()
+nodes = list(G_int.nodes)
+
+# remove self edges from network
+G_int.remove_edges_from(nx.selfloop_edges(G_int))
+
+# print out the numbers of nodes and edges in the interatome for diagnostic purposes:
+print('Number of nodes:', len(G_int.nodes))
+print('\nNumber of edges:', len(G_int.edges))
+
+int_nodes = list(G_int.nodes)
+
+# pre-calculate matrices used for network propagation. this step takes a few minutes, more for denser interactomes
+print('\ncalculating w_prime')
+w_prime = netprop.get_normalized_adjacency_matrix(G_int, conserve_heat=True)
+
+print('\ncalculating w_double_prime')
+w_double_prime = netprop.get_individual_heats_matrix(w_prime, .5)
+
+# subset seed genes to those found in interactome
+print("Number of D1 genes:", len(D1_genes))
+D1_genes = list(np.intersect1d(D1_genes,int_nodes))
+print("Number of D1 genes in interactome:", len(D1_genes))
+
+# D1 network propagation
+print('\nCalculating D1 z-scores: ')
+z_D1, Fnew_D1, Fnew_rand_D1 = netprop_zscore.calculate_heat_zscores(w_double_prime, int_nodes, 
+                                                                    dict(G_int.degree), 
+                                                                    D1_genes, num_reps=1000,
+                                                                    minimum_bin_size=100)
+
+z_D1 = pd.DataFrame({'z':z_D1})
+
+z_D1.sort_values('z',ascending=False).head()
+
+# ----------- select thresholds for NetColoc -----------------
+zthresh=3 # default = 3
+
+# select the genes in the network intersection, make a subgraph
+
+G_prox = nx.subgraph(G_int,z_D1[z_D1['z']>zthresh].index.tolist()) 
+print("Nodes in proximal subgraph:", len(G_prox.nodes()))
+print("Edges in proximal subgraph:", len(G_prox.edges()))
+
+G_cosSim=network_colocalization.transform_edges(G_prox,method='cosine_sim',edge_weight_threshold=0.95)
+
+# compile dataframe of metadata for overlapping nodes
+node_df = pd.DataFrame(index=list(G_prox.nodes))
+node_df = node_df.assign(d1_seeds=0, d1_name=d1_name,)
+node_df.loc[list(np.intersect1d(D1_genes,node_df.index.tolist())), 'd1_seeds']=1
+node_df['z_d1']=z_D1.loc[list(G_prox.nodes)]['z']
+node_df['sum_seeds']=node_df['d1_seeds']
+
+node_df = node_df.sort_values('z_d1',ascending=False)
+node_df.head(15)
+
+	
+print("Nodes in overlap subgraph:", len(G_prox.nodes()))
+print("Edges in overlap subgraph:", len(G_prox.edges()))
+# Create cx format of overlap subgraph
+G_prox_cx = ndex2.create_nice_cx_from_networkx(G_prox)
+G_prox_cx.set_name(d1_name+'_NetColoc_subgraph') 
+for node_id, node in G_prox_cx.get_nodes():
+    data = node_df.loc[node['n']]
+    for row, value in data.items():
+        if row == 'd1_seeds' or row == 'd2_seeds' or row=='sum_seeds':
+            data_type = 'double'
+        elif row=='d1_name' or row=='d2_name':
+            data_type='string'
+        else:
+            data_type = 'double'
+        G_prox_cx.set_node_attribute(node_id, row, value, type=data_type)
+
+cd = cdapsutil.CommunityDetection()
+
+# Run HiDeF on CDAPS REST service
+G_hier = cd.run_community_detection(G_prox_cx, algorithm='hidefv1.1beta',arguments={'--maxres':'20'})
+
+# Print information about hierarchy
+print('Hierarchy name: ' + str(G_hier.get_name()))
+print('# nodes: ' + str(len(G_hier.get_nodes())))
+print('# edges: ' + str(len(G_hier.get_edges())))
+
+G_hier = G_hier.to_networkx(mode='default')
+G_hier
+
+nodes = G_hier.nodes()
+
+# print the number of nodes and edges in the hierarchy for diagnostic purposes
+print('Number of nodes:', len(G_hier.nodes()))
+print('\nNumber of edges:', len(G_hier.edges()))
+
+	
+# add node attributes to dataframe for easier access
+hier_df = pd.DataFrame.from_dict(dict(G_hier.nodes(data=True)), orient='index')
+hier_df['system_ID']=hier_df.index.tolist()
+# some columns are not the right type
+hier_df['CD_MemberList_Size']=[int(x) for x in hier_df['CD_MemberList_Size'].tolist()]
+hier_df['HiDeF_persistence']=[int(x) for x in hier_df['HiDeF_persistence'].tolist()]
+hier_df.head()
+
+hier_df.index=hier_df['name']
+hier_df.head()
+
+num_d1_seeds = []
+frac_d1_seeds=[]
+
+systems_keep = []
+for c in hier_df.index.tolist():
+    system_genes = hier_df['CD_MemberList'].loc[c].split(' ')
+    d1_temp = list(np.intersect1d(system_genes,D1_genes))
+
+    num_d1_temp = len(d1_temp)
+    if (num_d1_temp)>0: # keep the system if it has at least 1 seed gene
+        systems_keep.append(c)
+        num_d1_seeds.append(num_d1_temp)
+        
+        frac_d1_seeds.append((num_d1_temp)/float(len(system_genes)))
+
+        
+frac_no_seeds = np.subtract(1.0,np.array([frac_d1_seeds]).sum(axis=0))
+
+hier_df = hier_df.loc[systems_keep]
+hier_df['num_d1_seeds']=num_d1_seeds
+hier_df['frac_d1_seeds']=frac_d1_seeds
+hier_df['frac_no_seeds']=frac_no_seeds
+print("Number of nodes with seed genes:", len(hier_df))
+
+hier_df.head()
+
+	
+# prune G_hier--> only keep systems with at least one seed gene
+
+nkeep=[]
+for n in list(G_hier.nodes()):
+    if G_hier.nodes(data=True)[n]['name'] in systems_keep:
+        nkeep.append(n)
+        
+
+G_hier = nx.subgraph(G_hier, nkeep)
+print("Number of nodes with seed genes:", len(G_hier.nodes()))
+print("Number of edges remaining:", len(G_hier.edges()))
+
+	
+# gprofiler annotation of clusters
+
+system_name_list = []
+for p in hier_df.index.tolist():
+    focal_genes=hier_df['CD_MemberList'].loc[p].split(' ')
+    print(p)
+    print(len(focal_genes))
+    if len(focal_genes)>2:
+        gp_temp = pd.DataFrame(gp.profile(focal_genes,significance_threshold_method='fdr',
+                                               sources=['REAC']))
+        if len(gp_temp)>0: # make sure data is not empty
+            
+            # make sure terms are specific, and overlap with at least 3 genes
+            gp_temp = gp_temp[(gp_temp['term_size']<1000)&(gp_temp['term_size']>50)]
+            gp_temp = gp_temp[gp_temp['intersection_size']>=3]
+            
+            gp_temp = gp_temp[gp_temp['p_value']<1E-5] # set a stringent pvalue threshold
+            
+            gp_temp = gp_temp.sort_values('recall',ascending=False)
+            
+            if len(gp_temp)>1:
+                system_name_list.append(gp_temp.head(1)['name'].tolist()[0])
+            else:
+                system_name_list.append(p)
+        else:
+            system_name_list.append(p)
+            
+        display(gp_temp.head())
+        
+    else:
+        system_name_list.append(p)
+
+	
+mgi_df = validation.load_MGI_mouseKO_data()
+mgi_df.head()
+
+	
+MPO = validation.load_MPO()
+MPO
+
+	
+def get_MP_description(term_id, ontology=MPO):
+    return ontology.node_attr.loc[term_id, "description"]
+
+	
+# find terms related to brain
+# ---- modify this part as needed for your project -----
+MP_focal_brain_list = []
+for t in MPO.node_attr.index.tolist():
+    descr_temp = MPO.node_attr.loc[t]['description']
+    if descr_temp.find('nervous')>-1:
+        MP_focal_brain_list.append(t)
+    elif descr_temp.find('neuron')>-1:
+        MP_focal_brain_list.append(t)
+    elif descr_temp.find('synapt')>-1:
+        MP_focal_brain_list.append(t)
+        
+print("Number of brain phenotypes", len(MP_focal_brain_list))
+print("Example brain phenotypes:")
+print("\n".join([mp+" - "+get_MP_description(mp) for mp in MP_focal_brain_list[0:10]]))
+
+	
+# add a negative control phenotype: abnormal innate immunity: MP:0002419
+# negative controls are tough here because we're dealing with development, which impacts almost everything.
+MP_focal_list = ['MP:0002419']+MP_focal_brain_list
+root_KO_brain_df=validation.MPO_enrichment_root(hier_df,MPO,mgi_df,MP_focal_list,G_int,verbose=True)
+
+	
+root_KO_brain_df.head()
+	
+# join brain and heart results together
+root_KO_brain_df['MPO_term_type']='brain'
+root_KO_df = root_KO_brain_df
+root_KO_df.loc['MP:0002419','MPO_term_type'] = 'neg_ctrl'
+root_KO_df = root_KO_df.sort_values('OR_p')
+root_KO_df.head()
+root_KO_df['MPO_term_type'].value_counts()
+
+	
+# plot top performing brain and heart terms + negative control term (MP:0002419) (for terms which have at least 150 genes)
+
+brain_terms_plot = root_KO_brain_df[root_KO_brain_df['num_genes_in_term']>150]
+brain_terms_plot = brain_terms_plot.sort_values('OR_p',ascending=True).head(5).index.tolist() #+brain_terms_plot.sort_values('OR_p',ascending=True).tail(5).index.tolist()
+
+neg_ctrl_terms_plot=['MP:0002419']
+
+terms_plot = brain_terms_plot +neg_ctrl_terms_plot
+
+	
+plt.figure(figsize=(3,6))
+
+plt.errorbar(root_KO_df.loc[terms_plot]['log_OR'],np.arange(len(terms_plot)),
+            xerr=[np.subtract(root_KO_df.loc[terms_plot]['log_OR'],root_KO_df.loc[terms_plot]['log_OR_CI_lower']),
+                   np.subtract(root_KO_df.loc[terms_plot]['log_OR_CI_upper'],root_KO_df.loc[terms_plot]['log_OR'])],color='k',fmt='o')
+
+color_temp = root_KO_df.loc[terms_plot]['MPO_term_type'].map({'brain':'blue','heart':'red','neg_ctrl':'black'})
+
+sns.scatterplot(x=root_KO_df.loc[terms_plot]['log_OR'],
+                y=np.arange(len(terms_plot)),size=root_KO_df.loc[terms_plot]['num_genes_in_term'],sizes=(200, 2000),
+                alpha=.5,
+               hue=color_temp.tolist(),palette={'blue':'blue','red':'red','black':'black'},legend=False)
+
+plt.yticks(np.arange(len(terms_plot)),root_KO_df.loc[terms_plot]['MP_description'])
+plt.xticks([0,1,2])
+plt.xlabel('log(OR) +- 95% CI')
+
+plt.plot([0,0],[-.8,len(terms_plot)-.5],'--',color='gray')
+plt.ylim([-0.8,len(terms_plot)-.5])
+
+plt.gca().invert_yaxis()
+
